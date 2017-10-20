7d3450fc68c9aa9cad17f5ed08e7097360adde22
Csy <274322346@qq.com>
2017-10-20 23:10:20 +0800

    添加项目文件。

---
+++ LGClientTest/ServiceCallback.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LGClientTest
+{
+    public class ServiceCallback : WcfService.IWcfServiceCallback
+    {
+        public void ClientOffline(string Msg)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
